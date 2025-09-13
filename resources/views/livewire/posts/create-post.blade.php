207f799f36c95b11660e13503bd100cbc13a374e
Kennedy Gitahi <kengitahi@hotmail.com>
2025-09-13 13:26:34 +0300

    Create database connections

--- resources/views/livewire/posts/create-post.blade.php
+++ resources/views/livewire/posts/create-post.blade.php
@@ -39,6 +39,7 @@
             :class="isOnline ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'"
             x-show.transition.duration.500ms="showMessage"
             x-cloak
+            x-transition
         >
             <span
                 class="font-semibold"
